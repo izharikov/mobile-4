9f0ee47e3eae64b7755abbb4a0167668c1666b76
Igor <zharikovigor97@gmail.com>
2016-11-27 17:17:10 +0300

    lab done

---
+++ BelarusCatalog2/app/src/main/java/com/example/igor/belaruscatalog/db/sort/SortOption.java
@@ -0,0 +1,31 @@
+package com.example.igor.belaruscatalog.db.sort;
+
+/**
+ * @author Ihar Zharykau
+ */
+public enum SortOption {
+    ASC, DESC;
+
+    public SortOption revert(){
+        switch (this){
+            case ASC:
+                return DESC;
+            case DESC:
+                return ASC;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        switch (this){
+            case ASC:
+                return "ASC";
+            case DESC:
+                return "DESC";
+            default:
+                return "";
+        }
+    }
+}
